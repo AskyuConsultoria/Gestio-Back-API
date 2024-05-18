bf2022f1d4abca93543dd7a53542019baaf524c0
kayky <kayky.pedroso@sptech.school>
2024-05-18 14:07:53 -0300

    feat: adição de descrições para o Swagger

--- src/main/kotlin/consultoria/askyu/gestio/controller/PecaController.kt
+++ src/main/kotlin/consultoria/askyu/gestio/controller/PecaController.kt
@@ -3,6 +3,11 @@ package consultoria.askyu.gestio.controller
 import consultoria.askyu.gestio.dominio.Peca
 import consultoria.askyu.gestio.dtos.PecaCadastroRequest
 import consultoria.askyu.gestio.service.PecaService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -13,30 +18,72 @@ class PecaController(
     var pecaService: PecaService,
 ) {
 
+    @Operation(summary = "Cadastra um peça com base no Id do usuário.",
+        description = "Realiza um cadastro de uma peça de roupa.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Cadastro feito com sucesso!"),
+            ApiResponse(responseCode = "404", description = "Usuário não existe.", content = [Content(schema = Schema())])
+        ],
+    )
     @PostMapping("/{id}")
     fun postByUsuarioId(@Valid @PathVariable id: Int, @RequestBody novaPeca: PecaCadastroRequest): ResponseEntity<Peca>{
      val peca  = pecaService.postByUsuarioId(id, novaPeca)
         return ResponseEntity.status(201).body(peca)
     }
 
+    @Operation(summary = "Busca todos as peças pelo Id do usuário.",
+        description = "Retorna todas as peças pelo Id do usuário.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Peças buscadas com sucesso!"),
+            ApiResponse(responseCode = "204", description = "Nenhuma peça cadastrada no sistema."),
+            ApiResponse(responseCode = "404", description = "Usuário não existe.", content = [Content(schema = Schema())])
+        ],
+    )
     @GetMapping("/{id}")
     fun getAllByUsuarioId(@PathVariable id: Int): ResponseEntity<List<Peca>>{
        val listaPeca = pecaService.getAllByUsuarioId(id)
         return ResponseEntity.status(200).body(listaPeca)
     }
 
+    @Operation(summary = "Busca uma peça com base no Id do usuário e da peça.",
+        description = "Retorna uma única peça com base no Id de um usuário e de uma peça.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Peças encontrada com sucesso!"),
+            ApiResponse(responseCode = "404", description = "Usuário ou Peça não existem.", content = [Content(schema = Schema())])
+        ],
+    )
     @GetMapping("/{usuarioId}/{pecaId}")
     fun getByUsuarioIdAndId(@PathVariable usuarioId: Int, @PathVariable pecaId: Int): ResponseEntity<Peca>{
         val peca = pecaService.getByUsuarioIdAndId(usuarioId, pecaId)
         return ResponseEntity.status(200).body(peca)
     }
 
+    @Operation(summary = "Busca todas as peças com base no Id do usuário e o nome da peça.",
+        description = "Retorna toda as peças com base no Id do usuário e no nome da peça, ignorando o case.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Peças encontradas com sucesso!"),
+            ApiResponse(responseCode = "204", description = "Nenhuma peça corresponde ao nome usado."),
+            ApiResponse(responseCode = "404", description = "Usuário não existe.", content = [Content(schema = Schema())])
+        ],
+    )
     @GetMapping("/{id}/filter-nome")
     fun getByUsuarioIdAndNome(@PathVariable id: Int, @RequestParam nome: String): ResponseEntity<List<Peca>>{
         val listaPeca = pecaService.getByUsuarioIdAndNome(id, nome)
         return ResponseEntity.status(200).body(listaPeca)
     }
 
+    @Operation(summary = "Atualiza uma peça com base no Id do usuário e peça.",
+        description = "Atualiza uma peça com base no Id do usuário e da peça selecionada.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Peça atualizada com sucesso!"),
+            ApiResponse(responseCode = "404", description = "Usuário ou Peça não existem.", content = [Content(schema = Schema())])
+        ],
+    )
     @PutMapping("/{usuarioId}/{pecaId}")
     fun putByUsuarioIdAndId(
         @PathVariable usuarioId: Int,
@@ -47,6 +94,15 @@ class PecaController(
         return ResponseEntity.status(200).body(peca)
     }
 
+
+    @Operation(summary = "Desativa uma peça com base no Id do usuário e peça.",
+        description = "Desativa uma peça com base no Id do usuário e da peça selecionada.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "204", description = "Peça desativada com sucesso!", content = [Content(schema = Schema())]),
+            ApiResponse(responseCode = "404", description = "Usuário ou Peça não existem.", content = [Content(schema = Schema())])
+        ],
+    )
     @DeleteMapping("/{usuarioId}/{pecaId}")
     fun deleteByUsuarioIdAndId(@PathVariable usuarioId: Int, @PathVariable pecaId: Int): ResponseEntity<Void>{
         pecaService.deleteByUsuarioIdAndId(usuarioId, pecaId)
