8bd6d7aa3af21b4aae0f9cf5b7258bd0bcfb9358
kayky <kayky.pedroso@sptech.school>
2024-05-18 23:51:26 -0300

    feat: adição da descrição do swagger

--- src/main/kotlin/consultoria/askyu/gestio/controller/NomeMedidaController.kt
+++ src/main/kotlin/consultoria/askyu/gestio/controller/NomeMedidaController.kt
@@ -3,6 +3,11 @@ package consultoria.askyu.gestio.controller
 import consultoria.askyu.gestio.dominio.NomeMedida
 import consultoria.askyu.gestio.dtos.NomeMedidaCadastroRequest
 import consultoria.askyu.gestio.service.NomeMedidaService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -13,6 +18,15 @@ class NomeMedidaController(
     var nomeMedidaService: NomeMedidaService
 )
  {
+
+     @Operation(summary = "Cadastra um nome de medida com base no Id do usuário e da peça.",
+         description = "Realiza um cadastro de um nome de medida de roupa com base no Id de usuário e da peça.")
+     @ApiResponses(
+         value = [
+             ApiResponse(responseCode = "200", description = "Cadastro feito com sucesso!"),
+             ApiResponse(responseCode = "404", description = "Usuário ou Peça não existem.", content = [Content(schema = Schema())])
+         ],
+     )
      @PostMapping("{usuarioId}/{pecaId}")
      fun postByUsuarioIdAndPecaId(
          @Valid
@@ -24,6 +38,16 @@ class NomeMedidaController(
          return ResponseEntity.status(200).body(nomeMedida)
      }
 
+
+     @Operation(summary = "Busca todos os nomes de medidas pelo Id do usuário e da peça.",
+         description = "Retorna todas os nomes de medidas pelo Id do usuário e da peça.")
+     @ApiResponses(
+         value = [
+             ApiResponse(responseCode = "200", description = "Nomes de medidas buscadas com sucesso!"),
+             ApiResponse(responseCode = "204", description = "Nenhum nome de medida cadastrada no sistema."),
+             ApiResponse(responseCode = "404", description = "Usuário ou Peça não existem.", content = [Content(schema = Schema())])
+         ],
+     )
     @GetMapping("{usuarioId}/{pecaId}")
     fun getAllByUsuarioIdAndPecaId(
         @PathVariable usuarioId: Int,
@@ -33,6 +57,15 @@ class NomeMedidaController(
         return ResponseEntity.status(200).body(listaNomeMedida)
     }
 
+     @Operation(summary = "Busca todos os nomes de medidas pelo Id do usuário e da peça e pelo nome.",
+         description = "Retorna todas os nomes de medidas pelo Id do usuário e da peça e pelo nome da medida.")
+     @ApiResponses(
+         value = [
+             ApiResponse(responseCode = "200", description = "Nomes de medidas buscadas com sucesso!"),
+             ApiResponse(responseCode = "204", description = "Nenhum nome de medida cadastrada no sistema."),
+             ApiResponse(responseCode = "404", description = "Nenhum nome de medida foi encontrada com este nome.", content = [Content(schema = Schema())])
+         ],
+     )
      @GetMapping("{usuarioId}/{pecaId}/filter-nome")
      fun getAllByUsuarioIdAndPecaIdAndNome(
          @PathVariable usuarioId: Int,
@@ -43,6 +76,14 @@ class NomeMedidaController(
          return ResponseEntity.status(200).body(listaNomeMedida)
      }
 
+     @Operation(summary = "Busca um nome de medida pelo Id do usuário, da peça e da própria medida.",
+         description = "Retorna um nome de medida pelo Id do usuário, da peça e da própria medida.")
+     @ApiResponses(
+         value = [
+             ApiResponse(responseCode = "200", description = "Nome de medida econtrada com sucesso!"),
+             ApiResponse(responseCode = "404", description = "Nome de medida não foi encontrado.", content = [Content(schema = Schema())])
+         ],
+     )
      @GetMapping("{usuarioId}/{pecaId}/{nomeMedidaId}")
          fun getByUsuarioIdAndPecaIdAndId(
              @PathVariable usuarioId: Int,
@@ -54,7 +95,14 @@ class NomeMedidaController(
          }
 
 
-
+     @Operation(summary = "Atualiza um nome de medida pelo Id do usuário, da peça e da própria medida.",
+         description = "Atualiza um nome de medidas pelo Id do usuário, da peça e da própria medida.")
+     @ApiResponses(
+         value = [
+             ApiResponse(responseCode = "200", description = "Nome de medida atulizado com sucesso!"),
+             ApiResponse(responseCode = "404", description = "Nome de medida não foi encontrado.", content = [Content(schema = Schema())])
+         ],
+     )
      @PutMapping("{usuarioId}/{pecaId}/{nomeMedidaId}")
      fun putByUsuarioIdAndPecaIdAndId(
          @PathVariable usuarioId: Int,
@@ -68,6 +116,14 @@ class NomeMedidaController(
          return ResponseEntity.status(200).body(nomeMedida)
      }
 
+     @Operation(summary = "Desativa um nome de medida pelo Id do usuário, da peça e da própria medida.",
+         description = "Realiza a exclusão lógica de um nome de medida pelo Id do usuário, da peça e da própria medida.")
+     @ApiResponses(
+         value = [
+             ApiResponse(responseCode = "200", description = "Nome de medida desativado com sucesso!"),
+             ApiResponse(responseCode = "404", description = "Nome de medida não foi encontrado.", content = [Content(schema = Schema())])
+         ],
+     )
      @DeleteMapping("{usuarioId}/{pecaId}/{nomeMedidaId}")
      fun deleteByUsuarioIdAndPecaIdAndId(
          @PathVariable usuarioId: Int,
