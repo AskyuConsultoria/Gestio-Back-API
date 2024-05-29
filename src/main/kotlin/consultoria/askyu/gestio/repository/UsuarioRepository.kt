298169d55f291bce5ae5af2347486fade954a811
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2024-05-29 20:00:27 -0300

    Criação dos testes de usuario

--- src/main/kotlin/consultoria/askyu/gestio/repository/UsuarioRepository.kt
+++ src/main/kotlin/consultoria/askyu/gestio/repository/UsuarioRepository.kt
@@ -7,6 +7,8 @@ interface UsuarioRepository: JpaRepository<Usuario, Int> {
 
     fun countByUsuario(usuario: String): Int
 
+    fun countUsuarioAndSenhaAndAtivoIsTrue(usuario: String, senha: String): Int
+
     fun findByUsuarioAndSenha(usuario: String, senha: String): Usuario
 
     fun findByUsuario(usuario: String): Usuario
--- src/main/kotlin/consultoria/askyu/gestio/service/UsuarioService.kt
+++ src/main/kotlin/consultoria/askyu/gestio/service/UsuarioService.kt
@@ -1,10 +1,11 @@
 package consultoria.askyu.gestio.service
 
 import consultoria.askyu.gestio.dominio.Usuario
-import consultoria.askyu.gestio.dtos.UsuarioDTO
+import consultoria.askyu.gestio.dtos.UsuarioCadastroDTO
 import consultoria.askyu.gestio.repository.UsuarioRepository
 import org.modelmapper.ModelMapper
 import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 
@@ -19,37 +20,46 @@ class UsuarioService(
         }
     }
 
-    fun existenceValidation(id:Int){
-        if(!repository.existsById(id)) {
-            throw ResponseStatusException(HttpStatusCode.valueOf(404), "Usuario não encontrado!")
+    fun existenceValidation(id:Int):Boolean{
+        if(repository.existsById(id)) {
+            return true
         }
+        throw ResponseStatusException(HttpStatusCode.valueOf(404), "Usuario não encontrado!")
     }
 
-    fun validadeUnico(usuario: Usuario){
-        if(repository.countByUsuario(usuario.usuario) >= 1){
-            throw ResponseStatusException(HttpStatusCode.valueOf(409), "Esse cadastro ja existe no sistema!")
+    fun uniqueValidation(usuario: Usuario):Boolean{
+        if(repository.countByUsuario(usuario.usuario) == 0){
+            return true
         }
+        throw ResponseStatusException(HttpStatusCode.valueOf(409), "Esse cadastro ja existe no sistema!")
     }
 
-    fun cadastrar(usuario: UsuarioDTO):Usuario{
-        var usuario = mapper.map(usuario, Usuario::class.java)
-        return repository.save(usuario)
+    fun cadastrar(usuario: UsuarioCadastroDTO):Usuario{
+        val novoUsuario = mapper.map(usuario, Usuario::class.java)
+        uniqueValidation(novoUsuario)
+        return repository.save(novoUsuario)
     }
 
-    fun desativarUsuario(id:Int){
+    fun desativarUsuario(id:Int):Usuario{
         existenceValidation(id)
         var usuario = repository.findById(id).get()
         usuario.ativo = false
-        repository.save(usuario)
+        return repository.save(usuario)
     }
 
     fun logar(usuario:String, senha:String):Usuario{
-        var user = repository.findByUsuarioAndSenha(usuario, senha)
-        if(user.autenticado){
-            throw ResponseStatusException(HttpStatusCode.valueOf(400), "Esse usuario ja esta logado em algum outro lugar!")
+        if(repository.countUsuarioAndSenhaAndAtivoIsTrue(usuario, senha) == 1) {
+            var user = repository.findByUsuarioAndSenha(usuario, senha)
+            if (user.autenticado) {
+                throw ResponseStatusException(
+                    HttpStatusCode.valueOf(409),
+                    "Esse usuario ja esta logado em algum outro lugar!"
+                )
+            }
+            user.autenticado = true
+            return repository.save(user)
         }
-        user.autenticado = true
-        return repository.save(user)
+        throw ResponseStatusException(HttpStatusCode.valueOf(404), "Esse usuário não existe!")
     }
 
     fun deslogar(usuario: String):Usuario{
@@ -62,6 +72,7 @@ class UsuarioService(
     }
 
     fun obterInfo(id:Int):Usuario{
+        existenceValidation(id)
         return repository.findById(id).get()
     }
 
