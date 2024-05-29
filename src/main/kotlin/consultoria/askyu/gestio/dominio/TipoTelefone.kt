44cd75695afd9aac23594ee42562b382fd745ef0
sarinhaw <sarah.oliveira@sptech.school>
2024-05-29 20:21:11 -0300

    Feat(TipoTelefone):primeiros endpoints tipo telefone

---
+++ src/main/kotlin/consultoria/askyu/gestio/dominio/TipoTelefone.kt
@@ -0,0 +1,22 @@
+package consultoria.askyu.gestio.dominio
+
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+
+class TipoTelefone (
+
+    @field:Id
+    @field:GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id:Int,
+
+    var nome:String,
+
+    var digitos:Int,
+
+    @field:ManyToOne
+    var usuario: Usuario? = null
+
+) {
+}
